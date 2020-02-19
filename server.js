8982f0472998beb661c0063cb29ad1dd9c15e4ea
ron erlih <ronerlih@gmail.com>
2020-02-19 14:18:24 -0500

    serve static folder

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use(express.json());
 //   router.use("^/$", serverRenderer)
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   router.use(express.static(path.resolve(__dirname, 'client/build'), { maxAge: '30d' }));
-}
+// }
 
 // Add router (ssr and static)
 // app.use(router);
