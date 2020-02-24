26dee35874719c72e29f39d28558abff9a0095a6
ron erlih <ronerlih@gmail.com>
2020-02-24 16:06:55 -0500

    font end display

--- scripts/imageAnalysis.js
+++ scripts/imageAnalysis.js
@@ -14,7 +14,7 @@ module.exports = {
         return [
         // "https://images2.minutemediacdn.com/image/upload/c_crop,h_3236,w_5760,x_0,y_0/f_auto,q_auto,w_1100/v1554700227/shape/mentalfloss/istock-609802128.jpg"
         //    "https://upload.wikimedia.org/wikipedia/commons/1/16/HDRI_Sample_Scene_Balls_%28JPEG-HDR%29.jpg",
-        "https://2014.igem.org/wiki/images/a/a7/Sample.png",
+        "http://2014.igem.org/wiki/images/a/a7/Sample.png",
         // "https://www.passmark.com/source/img_posts/montest_slide_2.png",
         // "https://i.ytimg.com/vi/sr_vL2anfXA/maxresdefault.jpg",
         // "https://qph.fs.quoracdn.net/main-qimg-7e3c3f89920a527c3becb8e312b0a465",
