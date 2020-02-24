93525fbecc4f2bb24898ff5e71a4cbd9c0bd3d77
ron erlih <ronerlih@gmail.com>
2020-02-23 21:24:11 -0500

    edge and location, channel split and draw center points

--- scripts/imageAnalysis.js
+++ scripts/imageAnalysis.js
@@ -18,12 +18,22 @@ module.exports = {
     analyseImage: async function(image) {
         return new Promise(async (resolve, reject) => {
             setTimeout(async () => {
+                ///////-->
+                // paramaters and mats
                 const BLUR_SIZE = 3;
                 const THRESHHOLD = 127.5;
                 const CANNY_THRESHOLD = 100;
                 const EDGE_WEIGHT = 0.719;
+                const LOCATIONS_WEIGHT = 0.2;
+
                 const EDGE_GAMMA = 0;
 
+                const resultsOptions = {
+                    0: "red_channel",
+                    1: "green_channel",
+                    2: "blue_channel",
+                    3: "alpha_channel",
+                }
                 const moments = [];
                 const WeightedCentroid = [];
 
@@ -33,101 +43,102 @@ module.exports = {
                 // `jimpImage.bitmap` property has the decoded ImageData that we can use to create a cv:Mat
                 let src = cv.matFromImageData(jimpSrc.bitmap);
                 let dst = new cv.Mat();
-                let weightsMat = new cv.Mat();
+                // console.log(Object.keys(cv).filter(key => key.indexOf("INTER") >= 0));
+                let mask = new cv.Mat.ones(dst.rows, dst.cols, cv.CV_8U)
+                let weightsMat = src.clone();
                 let edgesMat = new cv.Mat();
+                // prettier-ignore
                 const locationMatrix = [
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    255,
-                    255,
-                    255,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0
+                    0,0,0, 0,0,0, 0,0,0,
+                    0,0,0, 255,255,255, 0,0,0,
+                    0,0,0, 0,0,0, 0,0,0
                 ];
-                let locationsMat = new cv.matFromArray(
-                    3,
-                    3,
-                    cv.CV_8UC3,
-                    locationMatrix
-                );
+                let locationsMat = new cv.matFromArray(3, 3, cv.CV_8UC3, locationMatrix);
                 saveImg(locationsMat, "locations");
 
                 // TO-DO: initaize with center of img point
                 let centerPoint = new cv.Point(0, 0);
-
-                // inverse
-                cv.bitwise_not(src, weightsMat);
-
+                
+                //rgb -> gray
+                cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
+                              
+                // inverse (initialize weightsMat)
+                // cv.bitwise_not(src, dst);
+                
+                // flip rgb -> bgr
+                // cv.cvtColor(dst, weightsMat, cv.COLOR_RGBA2BGR, 0);
+                                
                 // mat to gray, TO-DO: extract 3 channels
-                cv.cvtColor(weightsMat, dst, cv.COLOR_RGBA2GRAY, 0);
+                // cv.cvtColor(weightsMat, dst, cv.COLOR_BGR2GRAY, 0);
+
+
+                // cv.cvtColor(weightsMat, weightsMat, cv.COLOR_BGR2RBG, 0);
+                // saveImg(weightsMat, "weights")
 
                 // blur
                 // cv.blur(dst, dst, new cv.Size(BLUR_SIZE, BLUR_SIZE));
 
+                ///////-->
                 // rate edges
                 // canny eadge detection
                 cv.Canny(dst, edgesMat, CANNY_THRESHOLD, CANNY_THRESHOLD);
 
                 // threshold
-                cv.threshold(
-                    edgesMat,
-                    edgesMat,
-                    THRESHHOLD,
-                    255,
-                    cv.THRESH_BINARY
-                );
+                cv.threshold(edgesMat, edgesMat, THRESHHOLD,255, cv.THRESH_BINARY);
 
                 // cnvrt back bfr save
                 cv.cvtColor(edgesMat, edgesMat, cv.COLOR_GRAY2RGBA, 0);
                 saveImg(edgesMat, "edge");
+                // saveImg(weightsMat, "weightsMat");
 
-                await cv.addWeighted(
-                    weightsMat,
-                    1 - EDGE_WEIGHT,
-                    edgesMat,
-                    EDGE_WEIGHT,
-                    EDGE_GAMMA,
-                    weightsMat,
-                    -1
-                );
+                await cv.addWeighted(weightsMat,1 - EDGE_WEIGHT, edgesMat,EDGE_WEIGHT,EDGE_GAMMA,weightsMat,-1);
+                // saveImg(weightsMat, "weightsMatAfter");
 
+                ////rate locations
                 // locations
-                // cv.resize(locationsMat, weightsMat.cols, weightsMat.rows,function(img){
-                // cv.cvtColor(img, img, cv.COLOR_GRAY2RGBA, 0);
-                // saveImg(img, "loc")});
+                await cv.resize(locationsMat, locationsMat, weightsMat.size(),0,0,cv.INTER_LINEAR);
+                cv.cvtColor(locationsMat, locationsMat, cv.COLOR_RGB2RGBA, 0);
 
-                // saveImg(img, "location2");
-                let arr = new cv.moments(dst);
+                // saveImg(weightsMat, "weightsMatAfter");
+                //  async function(_locationsMat){
+                cv.addWeighted(weightsMat,1 - LOCATIONS_WEIGHT, locationsMat,LOCATIONS_WEIGHT,EDGE_GAMMA,weightsMat,-1);
+                
+                saveImg(weightsMat, "loc")
+            // });
 
-                centerPoint = new cv.Point(
-                    arr.m10 / arr.m00,
-                    arr.m01 / arr.m00
-                );
-                console.log(centerPoint);
-                // retur to rgba for display
-                cv.cvtColor(dst, dst, cv.COLOR_GRAY2RGBA, 0);
+                ////
+                //split img
+                let arrayofMats = [];
+                let results = {};
+                let vecOfMats = new cv.MatVector()
+                cv.split(weightsMat, vecOfMats);
+                for(let i = 0; i<4; i++){
+                    arrayofMats.push(vecOfMats.get(i))
+                }
+                arrayofMats.forEach((channel, i) => {
+                    // saveImg(img, "location2");
+                    let arr = new cv.moments(channel);
 
-                resolve("Center point: " + centerPoint.x);
+                    centerPoint = new cv.Point(
+                        arr.m10 / arr.m00,
+                        arr.m01 / arr.m00
+                    );
+                    cv.circle(src,centerPoint, 10, new cv.Scalar(0,255,0,0),4,cv.LINE_AA,0)
+
+                    centerPoint.x = centerPoint.x.toFixed(2)
+                    centerPoint.y = centerPoint.y.toFixed(2)
+                    results[resultsOptions[i]] = centerPoint;
+                    cv.cvtColor(channel, channel, cv.COLOR_GRAY2RGBA, 0);
+                    saveImg(channel, resultsOptions[i]);
+
+                    //DRAW ON SRC
+
+                })
+                saveImg(src,"src")
+                // // retur to rgba for display
+                // cv.cvtColor(dst, dst, cv.COLOR_GRAY2RGBA, 0);
+
+                resolve("Center point: x: " + JSON.stringify(results, null, 2));
             }, 1000);
             // resolve("Center point: " + centerPoint.x);
         });
