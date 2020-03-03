54dbf1c2b9976858758c300cd94e7bb024fc3d86
ron erlih <ronerlih@gmail.com>
2020-03-03 16:28:18 -0500

    multithred nodes

--- scripts/cluster.js
+++ scripts/cluster.js
@@ -5,8 +5,8 @@ module.exports = function () {
     const numCPUs = require('os').cpus().length;
     const clusterMap = {};
     // cluster mode
-    // const clusterflag = process.argv.indexOf('no-cluster') < 0 ? true : false;
-    const clusterflag = false;
+    const clusterflag = process.argv.indexOf('no-cluster') < 0 ? true : false;
+    // const clusterflag = false;
     if (clusterflag) {
       if (cluster.isMaster) {
         console.log(`Master ${process.pid} is running`);
