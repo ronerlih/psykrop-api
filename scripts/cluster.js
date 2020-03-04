bea357747afbdcf73af7d6c629b461d67c391b90
ron erlih <ronerlih@gmail.com>
2020-03-03 20:24:09 -0500

    workes by memory alloc

--- scripts/cluster.js
+++ scripts/cluster.js
@@ -2,7 +2,7 @@
 module.exports = function () {
     // cluster 
     const cluster = require('cluster');
-    const numCPUs = require('os').cpus().length;
+    // const numCPUs = require('os').cpus().length;
     const clusterMap = {};
     // cluster mode
     const clusterflag = process.argv.indexOf('no-cluster') < 0 ? true : false;
@@ -10,8 +10,11 @@ module.exports = function () {
     if (clusterflag) {
       if (cluster.isMaster) {
         console.log(`Master ${process.pid} is running`);
+        const WORKERS = process.env.WEB_CONCURRENCY || 1;
+
         // Fork workers.
-        for (let i = 0; i < numCPUs/2; i++) {
+        // for (let i = 0; i < numCPUs/2; i++) {
+          for (let i = 0; i < WORKERS; i++) {
           const worker = cluster.fork()
           clusterMap[worker.id] = i;
           worker.send({ cpu: worker.id });
