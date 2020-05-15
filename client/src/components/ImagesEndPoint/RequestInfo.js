5cf9b5054bd3cfdd4b5f64e83360ca867db025c5
ron erlih <ronerlih@gmail.com>
2020-05-15 19:39:29 -0400

    sync

--- client/src/components/ImagesEndPoint/RequestInfo.js
+++ client/src/components/ImagesEndPoint/RequestInfo.js
@@ -6,7 +6,7 @@ export default function() {
         <div>
             <h5>Request format</h5>
             <div className="code">POST /api/images?sort=[order]</div>
-            BODY (in json format below, excepts also url encoded, form, and text mime types)
+            BODY (in a json format - below, excepts also url encoded and text mime types)
             <JSONPretty id="json-pretty-body" valueStyle="color:white" data={{ images: ["url-to-img.file"] }}></JSONPretty>
         </div>
     );
