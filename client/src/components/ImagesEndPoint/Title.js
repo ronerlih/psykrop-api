23690293e88b72f77cb3239af03c35e6fada8a3f
ron erlih <ronerlih@gmail.com>
2020-05-17 17:30:02 -0400

    ui align

--- client/src/components/ImagesEndPoint/Title.js
+++ client/src/components/ImagesEndPoint/Title.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Col, Row } from "../reusable/Grid";
-export default function Title(){
-    return (
-        <div>
-            <Row>
-                <Col extraClass="endpoint-title">
-                    <h4 className="m-0" style={{display:"inline-block"}}>/api/images</h4>
-                    <span className="info-span"> endpoint</span>
-                    <p>A post request with an array of image urls will return an array of insights about each image.</p>
-                </Col>
-            </Row>
-        </div>
-    )
+export default function Title() {
+   return (
+      <div>
+         <Row>
+            <Col size="12">
+               <div className="endpoint-title">
+                  <h4 className="m-0" style={{ display: "inline-block" }}>
+                     /api/images
+                  </h4>
+                  <span className="info-span"> endpoint</span>
+                  <p>A post request with an array of image urls will return an array of insights about each image.</p>
+               </div>
+            </Col>
+         </Row>
+      </div>
+   );
 }
