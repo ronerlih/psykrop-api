bcf5f05f4f9b754c81d7be864fdfbf6e120f8278
ron erlih <ronerlih@gmail.com>
2020-05-25 11:59:14 -0400

    reuable endpoints titles, ui

--- client/src/components/VisualTestsForm/index.js
+++ client/src/components/VisualTestsForm/index.js
@@ -8,7 +8,7 @@ export default function(props) {
       <div>
          <Row>
             <Col size="md-6 ">
-               <Title info="endpoint" title="/api/visualTests" miniTitle="A GET request will return an array of insights from a predetermined set of images and will also provide visual output of the images in the set, the different channels rating, and the overall pixel ratings." />
+               <Title info="endpoint" title="/api/visualtests" miniTitle="A GET request will return an array of insights from a predetermined set of images and will also provide visual output of the images in the set, the different channels rating, and the overall pixel ratings." />
                {/* <h4>Visual tests</h4> */}
                <p>Get insight on a batch of preselected images to see results</p>
                <Button onclick={props.runTests}>Run visual tests</Button>
