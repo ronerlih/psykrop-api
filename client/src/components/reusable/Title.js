13937e9fb3c7fede85aa94783b9f2efbb574b3d6
ron erlih <ronerlih@gmail.com>
2020-06-23 15:26:46 -0400

    corners var

--- client/src/components/reusable/Title.js
+++ client/src/components/reusable/Title.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Col, Row } from "./Grid";
 import corner from "./corner.svg";
 
+const RENDER_CORNER = true;
+const Corner = () => RENDER_CORNER ?
+	<>
+		<img src={corner} style={{zIndex:2,right:0,bottom:-21,position:"absolute", height:20}}/>
+		<div style={{zIndex:2,right:0,bottom:-21,position:"absolute", height:20, width:"100%"}}/>
+	</> : <></>
+	
 export default function Title({ title, miniTitle, info }) {
 	return (
 		<Row style={{position:"relative"}}>
@@ -11,9 +18,7 @@ export default function Title({ title, miniTitle, info }) {
 					<h4 className="m-0" style={{ display: "inline-block" }}>
 						{title}
 					</h4>
-				<img src={corner} style={{zIndex:2,right:0,bottom:-21,position:"absolute", height:20}}/>
-				<div style={{zIndex:2,right:0,bottom:-21,position:"absolute", height:20, width:"100%"}}/>
-
+					<Corner />
 					<span className="info-span"> {info}</span>
 					<p>{miniTitle}</p>
 				</div>
